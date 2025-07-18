f0f1cf1bf95f54ec8020f281fc422146c8f4940c
PrinzKarneval <franz.schwerdtmann@gmail.com>
2025-07-18 21:29:48 +0200

    Initial

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+"""
+Unit tests for calculator
+"""
+import unittest
+
+import calculator
+
+
+class TestCalculator:
+    def test_add(self):
+        assert 3 == calculator.add(1, 2)
+
+    def test_subtract(self):
+        assert 2 == calculator.sub(3, 1)
+
+    def test_multiply(self):
+        assert 100 == calculator.multiply(10, 10)
